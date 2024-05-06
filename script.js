90fbd1c7bd237f11065f060333e2da38edfad8f6
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-05-05 19:58:12 -0700

    Add functionality to play rounds and place tokens
    Create addToken function in BoardCell to change that cell's current
    value. Create placeToken function in Gameboard to place the player's
    token onto the board. Write GameController to initialize two players.
    Give this function the ability to print the board each round, switch
    the player turn, and play a round.

--- script.js
+++ script.js
@@ -2,12 +2,11 @@
 ** The gameboard keeps track of the board state
 ** Only one instance is needed so it wrapped in an IIFE
 */
-
-const GameBoard = (function() {
+const Gameboard = (function() {
     const rows = 3;
     const columns = 3;
     const board = [];
-
+    // Create a 2D array to represent the board
     for (let i = 0; i < rows; i++) {
         board[i] = []
         for (let j = 0; j < columns; j++) {
@@ -15,29 +14,92 @@ const GameBoard = (function() {
         }
     }
 
-    const placeToken = (row, column, player) => {
-
+    // Place a token on the board by changing the cell's value
+    const placeToken = (row, column, playerToken) => {
+        // If that cell is already populated, the move is invalid
+        if (board[row][column].getValue()) return;
+        // Otherwise, place the token
+        board[row][column].addToken(playerToken);
     }
 
+    // Print the gameboard to the console
     const printBoard = () => {
-       const currentBoard = board.map((row) => row.map((square) => square.getValue()));
-       console.log(currentBoard);
+        // For each cell of each row, get the value and return a populated row
+        // Add the populated rows to the board
+        const currentBoard = board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(currentBoard);
     }
 
     return {
+        placeToken,
         printBoard
     }
-
 })();
 
+/*
+** A cell is one square on the board and consists of a value that
+** represents an empty spot, player 1's token, or player 2's token
+*/
 function BoardCell() {
     let value = 0;
-
+    // Change cell's value
+    const addToken = (playerToken) => {
+        value = playerToken;
+    }
+    // Retrieve cell's current value
     const getValue = () => value;
 
     return {
+        addToken,
         getValue
     }
 }
 
-GameBoard.printBoard();
+/*
+** GameController controls each turn of the game and
+** determines when there is a winner
+*/
+const GameController = (function(
+    player1 = "Player One", // Default parameters that can be changed
+    player1Token = "X",
+    player2 = "Player Two",
+    player2Token = "O"
+) {
+    const players = [
+        {
+            name: player1,
+            token: player1Token
+        },
+        {
+            name: player2,
+            token: player2Token
+        }
+    ]
+
+    let currentPlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    // Print the current board state and announce whose turn it is
+    const printNextRound = () => {
+        Gameboard.printBoard();
+        console.log(`${getCurrentPlayer().name}'s turn`);
+    }
+
+    const playRound = (row, column) => {
+        Gameboard.placeToken(row, column, getCurrentPlayer().token);
+        switchPlayerTurn();
+        printNextRound();
+    }
+
+    printNextRound();
+
+    return {
+        playRound
+    }
+})();
+
